888912486d6068538e71286c21f04794c8a07a0b
Daniel Hernandez <Daniel@Cennias-MBP.home>
2018-07-19 19:28:19 -0400

    Navbar
    Added Navbar to App

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-          <img src={logo} alt="logo" />
+        <Navbar/>
       </div>
     );
   }
---
+++ client/src/containers/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import logo from '../logo.svg';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+class Navbar extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <img src={logo} alt="logo" />
+            <Typography variant="title" color="inherit">
+              New
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Navbar);
