2b646585d267f8e545358e39acb9bc7fd4aead7e
Daniel Hernandez <Daniel@Cennias-MBP.home>
2018-07-19 19:53:30 -0400

    Navbar Tabs
    Added Navbar tabs for messages, schools and profile

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -3,8 +3,22 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import logo from '../logo.svg';
+
+const TabContainer = (props) => {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+};
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 const styles = {
   root: {
@@ -13,8 +27,18 @@ const styles = {
 };
 
 class Navbar extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
     const { classes } = this.props;
+    const { value } = this.state;
+
     return (
       <div className={classes.root}>
         <AppBar position="static" color="default">
@@ -24,7 +48,15 @@ class Navbar extends Component {
               New
             </Typography>
           </Toolbar>
+          <Tabs value={value} onChange={this.handleChange}>
+            <Tab label="Messages" />
+            <Tab label="Schools" />
+            <Tab label="Profile" href="#basic-tabs" />
+          </Tabs>
         </AppBar>
+        {value === 0 && <TabContainer>Messages</TabContainer>}
+        {value === 1 && <TabContainer>Schools</TabContainer>}
+        {value === 2 && <TabContainer>Profile</TabContainer>}
       </div>
     );
   }
